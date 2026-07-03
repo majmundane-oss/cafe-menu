76870484f92fc52de5222946a54b5d32e91da208
majmundane-oss <majmundane@gmail.com>
2026-07-03 08:10:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,158 @@
+const menu = {
+  "Kafe i topli napici": [
+    ["Espresso", "220"],
+    ["Dupli espresso", "300"],
+    ["Espresso sa mlekom", "200"],
+    ["Espresso sa biljnim mlekom", "220"],
+    ["Latte Macchiato", "220"],
+    ["Latte sa ukusom", "290", "pistać, vanila, karamela, lešnik"],
+    ["Ice Coffee", "220"],
+    ["Ice Coffee sa ukusom", "290", "pistać, vanila, karamela, lešnik"],
+    ["Matcha", "250"],
+    ["Matcha sa ukusom", "300", "jagoda, mango"],
+    ["Nescafe", "190"],
+    ["Domaća kafa", "160"],
+    ["Čaj Stassen", "190"],
+    ["Topla čokolada", "200"],
+    ["Veliki plazma šejk", "350"],
+    ["Mali plazma šejk", "250"]
+  ],
+
+  "Vode": [
+    ["Rosa", "170"],
+    ["Rosa gazirana", "170"],
+    ["Romerquelle limunska trava", "220"]
+  ],
+
+  "Ceđeni sokovi": [
+    ["Limunada", "190"],
+    ["Limunada sa ukusom", "270", "jagoda, mojito mint, lubenica, malina, breskva, tropsko voće, mango, zova, kivi"],
+    ["Ledeni čaj", "270", "jagoda, malina, breskva, tropsko voće, mango, kivi, zelena jabuka"],
+    ["Ceđena pomorandža", "280"],
+    ["Ceđeni grejp", "280"],
+    ["Vitaminski mix", "330"],
+  ],
+
+  "Gazirani sokovi": [
+    ["Coca-Cola", "230"],
+    ["Coca Cola Zero", "230"],
+    ["Fanta", "230"],
+    ["Sprite", "230"],
+    ["Schweppes bitter lemon", "230"],
+    ["Schweppes tonic", "230"],
+    ["Schweppes pink grapefruit", "230"],
+    ["Cocta", "230"],
+    ["Orangina", "300"]
+  ],
+
+    "Negazirani sokovi": [
+    ["Next", "240", "jabuka, pomorandža, breskva, jagoda, šumsko voće"],
+    ["Fuze tea", "240"],
+    ["Cedevita ", "190", "pomorandža, limun, limun zova, mango-ananas, limunska trava"]
+  ],
+
+  "Energetska pića": [
+    ["Ultra", "220"],
+    ["Red Bull", "330"]
+  ],
+
+  "Točena piva": [
+    ["Heineken 0,5", "280"],
+    ["Heineken 0,35", "240"],
+    ["Birra Moretti 0,5", "260"],
+    ["Birra Moretti 0,25", "220"]
+  ],
+
+  "Flaširana piva": [
+    ["Heineken 0,25", "280"],
+    ["Birra Moretti 0,33", "270"],
+    ["Zaječarsko 0,33", "230"],
+    ["Blanc 0,33", "270"],
+    ["Corona 0,33", "390"]
+  ],
+
+  "Vina i cideri": [
+    ["Belo vino", "450"],
+    ["Crveno vino", "450"],
+    ["Rose vino", "450"],
+    ["Kupinovo vino", "450"],
+    ["Aspall", "300", "jabuka, malina"]
+  ],
+
+  "Žestoka pića i likeri": [
+    ["Šljiva Živanović", "190"],
+    ["Dunja Živanović", "190"],
+    ["Viljamovka Živanović", "190"],
+    ["Dunja Pevac", "230"],
+    ["Medovača Pevac", "230"],
+    ["Johnnie Walker Red", "280"],
+    ["Johnnie Walker Black", "400"],
+    ["Jameson", "280"],
+    ["Jack Daniels", "300"],
+    ["Smirnoff vodka", "270"],
+    ["Olmeca tequila", "260"],
+    ["Beefeater gin", "290"],
+    ["Hendrick’s gin", "400"],
+    ["Vinjak V.S.", "200"],
+    ["Gorki List", "200"],
+    ["Baileys", "300"],
+    ["Jagermeister", "270"],
+    ["Absinth", "270"]
+  ],
+
+  "Kokteli": [
+    ["Negroni", "500", "campari, gin, vermouth rosso"],
+    ["Old Fashioned", "500", "bourbon, sugar, angostura bitter"],
+    ["Whiskey Sour", "500", "bourbon, lemon, simple syrup, foam"],
+    ["Amaretto Sour", "500", "amaretto, bourbon, lemon, simple syrup, foam"],
+    ["Cuba Libre", "400", "rum, lime, cola"],
+    ["Margarita", "400", "tequila, triple sec, lime, agava syrup"],
+    ["Tequila Sunrise", "400", "tequila, grenadine, orange juice"],
+    ["Gin Tonic", "350", "gin, lime, cucumber, tonic water"],
+    ["Tom Collins", "350", "gin, lemon, simple syrup, soda"],
+    ["Skinny Bitch", "350", "vodka, lemon, soda"],
+    ["Long Island Ice Tea", "550", "gin, vodka, rum, tequila, triple sec, lemon, cola"],
+    ["Mai Tai", "450", "white rum, black rum, amaretto, triple sec, lime, pineapple juice, grenadine"],
+    ["Cosmopolitan", "400", "vodka, triple sec, lime, cranberry juice"],
+    ["Clover Club", "400", "gin, lemon, raspberry syrup, foam"],
+    ["Espresso Martini", "500", "vodka, espresso, kahlua, vanilla syrup"],
+    ["Porn Star Martini", "500", "vodka, passion fruit liquor, passion fruit purée, vanilla syrup, lime"],
+    ["Aperol Spritz", "450", "aperol, prosecco, soda"],
+    ["Campari Spritz", "450", "campari, prosecco, soda"],
+    ["Sarti Spritz", "450", "sarti, prosecco, soda"]
+  ]
+};
+
+
+const app = document.getElementById("app");
+
+function renderHome(){
+  app.innerHTML = `
+    <div class="grid">
+      ${Object.keys(menu).map(cat => `
+        <div class="card" onclick="openCategory('${cat}')">
+          ${cat}
+        </div>
+      `).join("")}
+    </div>
+  `;
+}
+
+function openCategory(cat){
+  const items = menu[cat];
+  app.innerHTML = `
+    <div class="back" onclick="renderHome()">← Nazad</div>
+    <h2>${cat}</h2>
+    ${items.map(i => `
+      <div class="item">
+        <div class="left">
+          <span class="name">${i[0]}</span>
+          ${i[2] ? `<span class="flavours">${i[2]}</span>` : ""}
+        </div>
+          <span class="price">${i[1]}</span>
+      </div>
+    `).join("")}
+  `;
+}
+
+renderHome();
